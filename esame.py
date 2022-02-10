97cec4f194eea72eb0829356aa19833b8c6bc455
MARSICULUS <91570512+MARSICULUS@users.noreply.github.com>
2022-02-10 11:50:14 +0000

    forse è finito

--- esame.py
+++ esame.py
@@ -21,27 +21,25 @@ get_data
 """
 class CSVFile:
 
+    #Inizializzazione
     def __init__(self, name):
-
-        #NOME del file
-        #Controlo Se il nome è una stringa che finisce per .csv
-        if type(name) == str and name[-4:] == '.csv':
-            t_input = True
-        else:
-            t_input = False
 
         #setto il nome
         self.name = name
         
-        #se il tipo del file è corretto
-        if not t_input:
+        #Controlo Se il nome è una stringa che finisce per .csv
+        if type(name) == str and name[-4:] == '.csv':
+            t_input = True
+        else:
             raise ExamException('Errore: il tipo del file non è .csv')
 
+            
     #Presentazione del file
     def __str__(self):
         return '[----------]\n{}\n[----------]'.format(self.name)
 
-    
+
+    #Leggere i dati
     def get_data(self):
 
         #controllo se il file esiste
@@ -51,29 +49,27 @@ class CSVFile:
         except Exception:
             raise ExamException('Errore: il file non può essere aperto (inesistente)')
 
-        #provo ad aprirlo
+        #controllo che il file non sia vuoto
         my_file = open(self.name, 'r')
-        #Controllo non sia vuoto
         vuoto = [1 for line in my_file if line != '\n' ]
         my_file.close()
-        #Se il file è vuoto raiso un Exception
         if sum(vuoto) < 1:
             raise ExamException('Errore: il file passato è vuoto')
 
+        
         #Prendo tutti i dati così come sono tranne le righe vuote
-        #Apro il file
         my_file = open(self.name, 'r')
-            
-        #creo una lista vuota (all data)
+        
         all_data = []
 
-        #Prendo tutte le righe e le metto nella lista
-        for line in my_file:            
-            #divido la riga per ogni colonna
+        for line in my_file:
             l = line.strip('\n')
             l = l.split(',')
-            #salvo la linea
-            if l[0] != '':
+            riga_vuota = True
+            for colonna in l:
+                if colonna != '':
+                    riga_vuota = False
+            if not riga_vuota:
                 all_data.append(l)
 
         my_file.close()
@@ -81,8 +77,6 @@ class CSVFile:
         return all_data
 
 class CSVTimeSeriesFile(CSVFile):
-    #cosa deve fare
-
     #devo alzare eccesioni quando: (funzione)
     #quando la serie temporale non è ordinata o ci sono dei doppioni
 
@@ -96,32 +90,37 @@ class CSVTimeSeriesFile(CSVFile):
     #prende il get data del super
     #converte le prime due colonnne in numeri e li aggiunge in una nuova lista
     #    (le colonne dopo non mi innteressano)
+
+    #Lista di liste formata da coppia data-intero
     def get_data(self):
         old_data = super().get_data()
-
         floaty_data = []
-    
+
+        #Per ogni lista, se ci sono due dati il primo lo aggiungo
+        #    il secondo lo converto in int
         for lista in old_data:
-            linea = []
-            for i, item in enumerate(lista):
-                if i == 0:
-                    linea.append(item)
-                if i == 1:
-                    try:
-                        floaty_item = int(item)
-                        if floaty_item < 0:
-                            raise Exception
-                        linea.append(floaty_item)
-                    except:
-                        linea.append(None)
+            if len(lista) >= 2:
+                
+                linea = []
+                for i, item in enumerate(lista):
+                    if i == 0:
+                        linea.append(item)
+                    if i == 1:
+                        try:
+                            floaty_item = int(item)
+                            if floaty_item < 0:
+                                raise Exception
+                            linea.append(floaty_item)
+                        except:
+                            linea.append(None)
+    
+                if linea[0] == 'date':
+                    pass
+                else:
+                    floaty_data.append(linea)
 
-            if linea[0] == 'date':
-                pass
-            else:
-                floaty_data.append(linea)
-
-        if not self.__time_check__(floaty_data):
-            raise ExamException('Errore: ordine temporale sbagliato')
+        #if not self.__time_check__(floaty_data):
+        #    raise ExamException('Errore: ordine temporale sbagliato')
                         
         return floaty_data
 
@@ -144,22 +143,58 @@ class CSVTimeSeriesFile(CSVFile):
                 l[1] = int(l[1])
                 right_data.append(l)
             except:
+                right_data.append(None)
+
+        #tutte le date
+        anni = []
+        for item in right_data:
+            try:
+                anni.append(item[0])
+            except:
                 pass
 
-        mese = 1
-        anno = right_data[0][0]
-        for item in right_data:
-            if item[0] != anno:
-                tutto_a_posto = False
-            if item[1] != mese:
-                tutto_a_posto = False
-            if mese == 12:
-                mese = 1
-                anno = anno + 1
+        #Controllo prima tutti gli anni
+        anni_a_posto = True
+        for i, item in enumerate(anni):
+            if i == len(anni) - 1:
+                pass
             else:
-                mese = mese + 1
+               try:
+                   if item > anni[i + 1]:
+                       anni_a_posto = False
+               except TypeError:
+                   pass
+                    
+        #tutti i mesi
+        mesi = []
+        for item in right_data:
+            try:
+                mesi.append(item[1])
+            except:
+                pass
 
-        return tutto_a_posto
+        #controllo tutti i mesi siano giusti
+        mesi_a_posto = True
+        for i, item in enumerate(mesi):
+            if i == len(mesi) - 1:
+                pass
+            else:
+                try:
+                    if mesi[i] >= mesi[i + 1]:
+                        if mesi[i] == mesi[i + 1]:
+                            mesi_a_posto = False
+                        else:
+                            if mesi[i] != 12:
+                                mesi_a_posto = False
+                           
+                except TypeError:
+                   pass
+
+        #return
+        if mesi_a_posto and anni_a_posto:
+            return True
+        else:
+            return False
 
 def compute_avg_monthly_difference(time_series, first_year, last_year):
 
@@ -184,40 +219,49 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
     modified_data = []
     for item in time_series:
         l = item[0].split('-')
-        l.append(item[1])
-        modified_data.append(l)
+        if len(l) == 2:
+            l.append(item[1])
+            modified_data.append(l)
 
+    
     data = []
     for item in modified_data:
         linea = []
+        add = True
         for i in range(2):
-            linea.append(int(item[i]))
-        linea.append(item[2])
-        data.append(linea)
+            try:
+                linea.append(int(item[i]))
+            except:
+                add = False
+        if add:
+            linea.append(item[2])
+            data.append(linea)
 
+    
     wanted_data = []
 
     for item in data:
         if item[0] >= first_year and item[0] <= last_year:
-            wanted_data.append(item[2])
+            wanted_data.append(item)
+   
 
-    nice_data = []
-
-    for i in range(0, len(wanted_data), 12):
-        nice_data.append(wanted_data[i:i+12])
-
-
+    passeggieri = []
+    for i in range(12):
+        mese = []
+        for item in wanted_data:
+            if item [1] == i + 1 and item[2] != None:
+                mese.append(item[2])
+        passeggieri.append(mese)
 
     difference_year = last_year - first_year + 1
     result = [0,0,0,0,0,0,0,0,0,0,0,0]    
 
-    for i in range(12):
-        for j, item in enumerate(nice_data):
-            if j + 1 < difference_year:
-                if item[i] != None and nice_data[j + 1][i] != None:
-                    result[i] = result [i] + abs(item[i] - nice_data[j + 1][i])
-
-            
+    for i, mesi in enumerate(passeggieri):
+        diff = 0
+        for j in range(len(mesi) - 1):
+            diff = diff + abs(mesi[j] - mesi[j + 1])
+        result[i] = diff
+    
     real_result = []
 
     for item in result:
@@ -226,56 +270,13 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
         else:
             real_result.append(item)
         
-    
-    '''
-    for i, item in enuemrate(data):
-        if item[0] >= first_year and item[0] <= last_year:
-            if item[2] != None:
-                result[item[1] - 1] = result[item[1] - 1] +  
-    
-    '''
-    
-    '''
 
-    for item in data:
-        if item[0] >= first_year and item[0] <= last_year:
-            print(item)
-            if item[2] != None:
-                result[item[1] - 1] = result[item[1] - 1] + item[2]
-
-    print(result)
-    print('---------')
-    
-    real_result = []
-
-    for item in result:
-        if item != 0:
-            real_result.append(item / difference_year)
-        else:
-            real_result.append(item)
-
-'''
     return real_result
-    #dovrei avere una lista come la volgio io (ci sono dei null nei dati dei passeggieri)
 
-    #faccio un altra funzione per cercare il dato dei passeggieri dato un anno e un giorno
 
-    #faccio un ciclo for che va da 0 a 11
-    #ogni ciclo dato il mese si trovano tutti i mesi e si sommano
-    
-    #only_passengers =[]
+file = CSVTimeSeriesFile(name = 'data.csv')
 
-    #for item in time_series:
-     #   only_passengers.append
-    
-    
-#====================
-# CORPO DEL PROGRAMMA
-#====================
-
-file = CSVTimeSeriesFile(name = 'file_vuoto.csv')
-#print(file.get_data())
-print(compute_avg_monthly_difference(file.get_data(), '1950', '1952'))
+print(compute_avg_monthly_difference(file.get_data(), '0', '2000'))
 
 
 #cose ancora da fare:
